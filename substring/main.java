4c031d30b56df6679c776506d528eace3811cee1
sakthivel-fullstackdeveloper <sakthivel2004it@gmail.com>
2025-01-29 09:12:57 +0530

    2nd week

---
+++ substring/main.java
@@ -0,0 +1,10 @@
+class ex{
+    public static void main(String[] args) {
+        String s1 ="one";
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = i+1; j <= s1.length(); j++) {
+            System.out.println(s1.substring(i, j));
+            }
+        }
+        }
+    }
