072ebdd6b53b6c100a01b6af617915e77a3e4cdd
sakthivel-fullstackdeveloper <sakthivel2004it@gmail.com>
2025-03-13 08:55:27 +0530

    Create pattern.java

---
+++ patterns_1/pattern.java
@@ -0,0 +1,23 @@
+class pattern{
+    public static void main(String[] args) {
+    //     for (int i = 1; i <=5; i++) {
+    //         for (int j = 1; j <=i; j++) {
+    //          System.out.print("*");
+    //         }
+    //         System.out.println();
+            
+    //     }
+    // }
+    int n=2;
+    for (int i = 0; i <n; i++) {
+        for (int j = 0; j <n-i-1; j++) {
+         System.out.print(" ");
+        }
+        for(int  k = 0;k<i*2+1;k++){
+            System.out.print("*");
+        }
+        System.out.println();
+        
+    }
+}
+}
